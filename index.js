9f3253e8b7f88a66ef97e53c9341418abf7e3bbd
Alvin-Karanja <alvin2@proton.me>
2023-10-27 15:00:59 +0300

    add related data

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
 
-// db
-import db from "./_db.js";
+// data
+import db from './_db.js'
 
 // types
-import { typeDefs } from "./schema.js";
+import { typeDefs } from './schema.js'
 
+// resolvers
 const resolvers = {
     Query: {
         games() {
@@ -27,7 +28,25 @@ const resolvers = {
         review(_, args) {
             return db.reviews.find((review) => review.id === args.id)
         }
-    }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.game_id === parent.id)
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find((a) => a.id === parent.author_id)
+        },
+        game(parent) {
+            return db.games.find((g) => g.id === parent.game_id)
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.author_id === parent.id)
+        }
+    },
 }
 
 // server setup
@@ -40,4 +59,4 @@ const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 }
 })
 
-console.log('Server ready at port', 4000)
+console.log(`Server ready at: ${url}`)
